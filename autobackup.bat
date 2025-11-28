45016ac09735185d682a102b1628635e2374c734
Zasikal114 <3572717551@qq.com>
2025-11-29 01:14:15 +0800

    auto backup 2025/11/29 周六  1:14:15.06

--- autobackup.bat
+++ autobackup.bat
@@ -4,7 +4,7 @@ echo starting backup...
 cd /d "C:\Users\lenovo\Desktop\jython"
 
 git add .
-git commit -m "自动备份: %date% %time%"
+git commit -m "auto backup %date% %time%"
 git push
 
 echo backup completed.
